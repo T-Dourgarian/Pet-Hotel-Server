236800c5e40e6bb121178d150beed8e8cff1540e
Thomas Dourgarian <Thomasdourgarian@gmail.com>
2019-12-05 16:51:01 -0600

    server side post route to db

--- app/controllers/api/pets_controller.rb
+++ app/controllers/api/pets_controller.rb
@@ -4,5 +4,20 @@ module Api
             pets = Pet.order('created_at DESC');
             render json: {status: 'SUCESS', message: 'Loaded pets', data: pets}, status: :ok
         end
+
+        def create
+            pets = Pet.new(article_params)
+
+            if pets.save
+                render json: {status: 'SUCESS', message: 'Pet added', data: pets}, status: :ok
+            else 
+                render json: {status: 'ERROR', message: 'Pet not added',data:article.errors},status: :unprocessable_entity
+            end
+        end
+
+        private
+        def article_params
+            params.permit(:owner_id,:name,:breed,:color,:checked_in,:checked_in_date)
+        end
     end
 end
