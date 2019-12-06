fcb0e4fca90bb8769d66064df92c1f4946c8a03b
Jon Barclay <my@nameisjon.com>
2019-12-05 19:45:18 -0600

    created required fields for db entries

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ApplicationRecord
+    validates :owner_id, presence: true
+    validates :name, presence: true
+    validates :breed, presence: true
+    validates :color, presence: true
 end
